2436b26e5fed1f86adeda7dac3fdaad9398bc94d
Nihar R Panda <110398042+NiharPanda23@users.noreply.github.com>
2024-02-24 20:30:52 +0530

    Data fatched and passed to the componet card component is completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,30 @@
 import NavBar from './component/NavBar';
 import HeaderContainer from './component/HeaderContainer';
 import CountryCard from './component/CountryCard';
-
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const App = () => {
+
+  const [countries, setCountries] = useState({})
+
+  useEffect(()=> {
+    axios.get("http://localhost:4000/getAllCountries")
+    .then(function(countries) {
+      setCountries(countries.data)
+      console.log(countries);
+    })
+    .catch(err => console.log(err))
+    
+  }, []);
+
+  // console.log(countries);
 
   return (
     <div className='lg:w-[1440px] lg:mx-auto bg-[#fafafa)]'>
       <NavBar/>
       <HeaderContainer />
-      <CountryCard />
+      <CountryCard  data={countries}/>
     </div>
   )
 } 
--- frontend/src/component/CountryCard.jsx
+++ frontend/src/component/CountryCard.jsx
@@ -1,39 +1,37 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
 
 
-const CountryCard = () => {
 
-  var [countries, setCountries] = useState([])
+const CountryCard = ({data}) => {
 
-  useEffect(()=> {
-    axios.get("http://localhost:4000/getAllCountries")
-    .then(function(countries) {
-      setCountries(countries.data)
-      console.log(countries);
-    })
-    .catch(err => console.log(err))
-    
-  }, []);
   
 
+  
+  console.log(data)
+
   return (
-    <div className='w-[1400px] mx-auto flex flex-col'>
+    <div className=' flex flex-wrap w-[1440px] mx-auto '>
+  {/* {
+
+    data.map((item)=> 
+      <h2>{item.name}</h2>
+    )
+  } */}
     {
-        countries.map(country =>{
-          <div className='border w-60 h-80 rounded-md shadow-xl mb-10'>
-              <div>
-                  <img className='rounded-t-md' src={country.flag} alt={country.name} />
-              </div>
-              <div>
-                  <h2 className='px-4 py-3 font-bold text-lg'>{country.name}</h2>
-                  <p className='px-4 py-0 leading-7'>Population:{country.population}</p>
-                  <p className='px-4 py-0 leading-7'>Region:{country.region}</p>
-                  <p className='px-4 py-0 leading-7'>Capital:{country.capital}</p>
-              </div>
-          </div>
-        })}
-    </div>
+      data.map((item)=>
+        <div className='border w-72 h-96 rounded-md shadow-xl mb-10 bg-white mx-8 mt-10 cursor-pointer hover:shadow-2xl'>
+            <div className="w-72 h-1/2">
+                <img className='rounded-t-md object-cover w-[100%] h-[100%]' src={item.flag}  alt={item.name}/>
+            </div>
+            <div className="w-72 h-1/2 mt-2">
+                <h2 className='px-6 py-3 font-bold text-lg'>{item.name}</h2>
+                <p className='px-6 py-0 leading-7'>Population : {item.population}</p>
+                <p className='px-6 py-0 leading-7'>Region : {item.region}</p>
+                <p className='px-6 py-0 leading-7'>Capital : {item.capital}</p>
+            </div>
+        </div>)
+    }
+        
+    </div>                                                                                               
   )
 }
 
